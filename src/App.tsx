55283089e8474bc4aec2cbff1484e06205c01e6d
Daniel <ccsnosovdg@gmail.com>
2023-12-05 13:59:33 +0300

    feat: init project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+import getAuthorsRequest from "./api/authors/getAuthorsRequest";
+import getCommentsRequest from "./api/comments/getCommentsRequest";
+
+function App() {
+    return <div className="App"></div>;
+}
+
+export default App;
